27429c42fbbbed99993e10100b02af5ad5b12173
Abdullah Al Noman <140447619+ablnoman58@users.noreply.github.com>
2023-08-11 17:04:28 +0600

    Add files via upload

---
+++ Function.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+//Declaration/prototype
+
+void printhello();
+void goodbye();
+//function call
+
+int main ()
+{
+
+    printhello();
+    goodbye();
+    return 0;
+}
+//function definition
+
+void printhello()
+
+{
+    printf("hello\n");
+}
+void goodbye()
+{
+    printf("goodbye\n");
+}
